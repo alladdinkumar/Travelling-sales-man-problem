486322c66825e09481e76ff6739552b0cc2897e0
alladdinkumar <31990974+alladdinkumar@users.noreply.github.com>
2018-10-16 01:03:30 +0530

    Update main_file.py
    adding the calculation algorithm to calculate the shortest path from the city adjacency matrix

--- main_file.py
+++ main_file.py
@@ -41,15 +41,117 @@ def matrix_copy_to_local():
     for i in range(5):
         city_mat.append([])
         for j in range(5):
-            city_mat[i].append(e[k].get())
+            city_mat[i].append(int(e[k].get()))
             k+=1
-    calculation()
+    TSP(city_mat)
             
-def calculation():
-    print("calculating....")
+
+
+
+#calculation algorithm starts-------------------------------------------------------------------------------------------------------------
+def copy_to_final(curr_path):
+    global final_path
+    for i in range(5):
+        final_path[i]=curr_path[i]
+    final_path[5]=curr_path[0]
+    
+
+def first_min(city_mat,i):
+    mini=float("inf")
+    for k in range(5):
+        if (city_mat[i][k]<mini and i!=k):
+            mini=city_mat[i][k]
+    return mini
+def secondmin(city_mat,i):
+    first=float("inf")
+    second=float("inf")
+    for j in range(5):
+        if(i==j):
+            continue
+        if(city_mat[i][j]<=first):
+            second=first
+            first=city_mat[i][j]
+        elif(city_mat[i][j]<=second and first!=city_mat[i][j]):
+            second=city_mat[i][j]
+    return second
+
+def tsprec(city_mat,curr_bound,curr_weight,level,curr_path):
+    
+    global final_res,visited,final_path
+    if level==5:
+        if(city_mat[curr_path[level-1]][curr_path[0]]!=0):
+            curr_res=curr_weight+city_mat[curr_path[level-1]][curr_path[0]]
+            #print(curr_res)
+            if(curr_res<final_res):
+                copy_to_final(curr_path)
+                final_res=curr_res
+                
+        return 
+
+    for i in range(5):
+        if(city_mat[curr_path[level-1]][i]!=0 and visited[i]==False):
+            temp=curr_bound
+            curr_weight+=city_mat[curr_path[level-1]][i]
+            if(level==1):
+                curr_bound-=((first_min(city_mat,curr_path[level-1])+first_min(city_mat,i))/2)
+            elif(level!=1):
+                curr_bound -= ((secondmin(city_mat, curr_path[level-1]) + 
+							first_min(city_mat, i))/2)
+            if(curr_bound+curr_weight< final_res):
+                curr_path[level]=i
+                visited[i] = True
+              
+                tsprec(city_mat,curr_bound, curr_weight, level+1,curr_path)
+            curr_weight -= city_mat[curr_path[level-1]][i]
+            curr_bound = temp
+            for l in range(5):
+                visited[l]= False
+            for j in range(level):
+                visited[curr_path[j]] = True
+def TSP(city_mat):
     
     
+    global final_res,visited,final_path
+    curr_path=[-1,-1,-1,-1,-1,-1]
+    curr_bound=0
+    for j in range(5):
+                visited[j] = 0
+	
+    for i in range(5):
+        curr_bound += (first_min(city_mat, i) + secondmin(city_mat, i))
+
+	
+    if (curr_bound %2 !=0):
+         curr_bound=curr_bound/2 + 1
+    else:
+         curr_bound=curr_bound/2
     
+    visited[0] = True
+    curr_path[0] = 0
+
+	
+    tsprec(city_mat,curr_bound, 0, 1,curr_path);
+
+    print("Minimum cost : %f\n"% final_res)
+    print("Path Taken : ")
+    for i in range(6) :
+        print("%d "% final_path[i])
+    
+
+
+
+final_path=[float("inf"),float("inf"),float("inf"),float("inf"),float("inf"),float("inf")]
+visited=[0,0,0,0,0]
+final_res=float("inf")
+#calculation algorithm ends--------------------------------------------------------------------------------------------------------------
+
+
+ 
+
+
+    
+
+  
 
         
 city=[]
@@ -57,8 +159,9 @@ l=list( i for i in range(5))
 c=list( i for i in range(5))
 city_mat=[]
 e=list( i for i in range(25))
-city_entry()
 
+city_entry()
+ 
 
     
     
